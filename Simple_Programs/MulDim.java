feeede17c5a8cb81ff85396b79f60448766faf60
Morty😎 <122627325+Narendrashivasai@users.noreply.github.com>
2025-09-05 16:38:39 +0530

    Add files via upload

--- Simple_Programs/MulDim.java
+++ Simple_Programs/MulDim.java
@@ -6,28 +6,27 @@ public class MulDim {
 
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter the number of rows");
-		int rows=sc.nextInt();
-		System.out.println("enter the number of columns");
-		int columns=sc.nextInt();
+		System.out.println("enter the number of rows");
+		int r=sc.nextInt();
+		System.out.println("enter the number of columns: ");
+		int c=sc.nextInt();
 		
-		int[][] matrix=new int[rows][columns];
+		int[][] arr=new int[r][c];
+		System.out.println("enter the elements: ");
 		
-		System.out.println("enter the array from the user");
-		for(int i=0;i<=rows;i++) {
-			for(int j=0;j<columns;j++) {
-				System.out.println("enter elements at row"+(i)+"'column"+(j)+":");
-				matrix[i][j]=sc.nextInt();
+		for(int i=0;i<r;i++) {
+			for(int j=0;j<c;j++) {
+				System.out.println("entert the element at row "+i+" columm "+j+" : ");
+			arr[i][j]=sc.nextInt();
 			}
-			
-			}
-		System.out.println("elements in 2d array");
-		for(int[] rows1 :matrix) {
-			for(int num: rows1) {
-				System.out.print(num+" ");
-			}
-			System.out.println();
 		}
+		System.out.println("entered elements are : ");
+		for(int[] row:arr) {
+			for(int num:row) {
+		
+		System.out.println(num);
+			}
+			}
 	}
 
 }
