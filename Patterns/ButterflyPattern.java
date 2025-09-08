7efb7af95152aa4a3cfdebdc67d5691328aec77e
Morty😎 <122627325+Narendrashivasai@users.noreply.github.com>
2025-09-08 17:53:56 +0530

    Add files via upload

--- Patterns/ButterflyPattern.java
+++ Patterns/ButterflyPattern.java
@@ -20,6 +20,24 @@ public class ButterflyPattern {
 			}
 			System.out.println();
 		}
+		
+		//lower part
+		for(int i=rows;i>=1;i--) {
+			
+			for(int j=1;j<=i;j++) {
+				System.out.print("* ");
+			}
+			
+			int spaces=2*(rows-i);
+			for(int j=1;j<=spaces;j++) {
+				System.out.print("  ");
+			}
+			for(int j=1;j<=i;j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+		
 
 	}
 
