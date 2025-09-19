243e074a374632ff2bf7fb276349444e3e7ff82d
Morty😎 <122627325+Narendrashivasai@users.noreply.github.com>
2025-09-19 21:02:24 +0530

    Add files via upload

---
+++ Methods/Box.java
@@ -0,0 +1,15 @@
+package com.codegnan.methods;
+
+public class Box {
+
+		double width;    // instance variables
+		double height;
+		double depth;
+		
+		public Box(double width, double height,double depth) {
+			super();
+			this.width = width;
+			this.height = height;
+			this.depth = depth;
+		}
+}
